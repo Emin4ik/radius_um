32c4eb2ee51d47062dc4a020a46e78144cbff97e
Emin Baghirov <eminbaghirov@Emins-MacBook-Pro.local>
2024-04-02 23:37:32 +0400

    mac

--- app/Http/Controllers/StoreController.php
+++ app/Http/Controllers/StoreController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Merchant;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class StoreController extends Controller
 {
@@ -12,7 +13,19 @@ class StoreController extends Controller
         $merchant = Merchant::findOrFail($id);
         session()->put('shop', $merchant['name']);
         session()->put('shop_id', $merchant['internal_id']);
+
+        // $products = DB::table('stores')
+        //     ->leftJoin('offers', 'stores.id', '=', 'offers.store_id')
+        //     ->select('stores.old_price', 'stores.retail_price', 'stores.discount', 'stores.img_url_thumbnail', 'stores.manufacturer', 'stores.score', 'stores.name', 'offers.retail_price', 'offers.old_price', 'offers.partner_rating', 'offers.name AS offer_name', 'offers.logo', 'offers.internal_id')
+        //     ->orderBy('stores.id')
+        //     ->get()
+        //     ->groupBy('id');
+        $products = DB::table('stores')
+            ->select('stores.old_price', 'stores.retail_price', 'stores.discount', 'stores.img_url_thumbnail', 'stores.manufacturer', 'stores.score', 'stores.name')
+            ->get();
+        // dd($products);
         return view( 'store.index', [
+                'products' => $products,
                 'merchant' => $merchant
             ]
         );
